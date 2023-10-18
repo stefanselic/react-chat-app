fc61b46edca938db46e52971c6c976e40031709e
stefanselic <s.stefan22@hotmail.com>
2023-10-18 14:06:40 +0200

    Add components, add ChatEngine, add environment variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 /** @format */
-
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './_components/ChatFeed';
+import LoginForm from './_components/LoginForm';
+
+const projectID = process.env.REACT_APP_ID_KEY;
 
 export default function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3',
+        ).play()
+      }
+    />
   );
 }
